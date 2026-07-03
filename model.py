9406e7c33bf80ff6932af32cf7140ba9c6ecdc46
Hammadi Rafik Ouariachi <115115206+OuariachiRafik@users.noreply.github.com>
2026-07-03 17:01:08 +0200

    Step 5: row_sum

--- model.py
+++ model.py
@@ -43,8 +43,16 @@ __global__ void row_max(const float* matrix, float* out, int rows, int cols) {
         out[r] = max;
 }
 
-# Step 5 - row_sum (not yet solved)
-# TODO: implement
+# Step 5 - row_sum
+__global__ void row_sum(const float* matrix, float* out, int rows, int cols) {
+    int r = blockIdx.x * blockDim.x + threadIdx.x;
+    if (r >= rows) return;
+    float sum = 0;
+    for (int c = 0; c < cols; c++){
+        sum += matrix[r*cols+c];
+        } 
+        out[r] = sum;
+}
 
 # Step 6 - dot_product (not yet solved)
 # TODO: implement
